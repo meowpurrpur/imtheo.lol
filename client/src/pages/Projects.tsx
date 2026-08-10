c5e6eadb8ffa73ea9d49eb57def92a0e84caeb03
meowpurrpur <contact@imtheo.lol>
2026-08-10 19:34:45 +0100

    remove

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import ProjectCard from "../components/ProjectCard";
 import useTitle from "../hooks/Title";
-import { FaCircleNotch, FaDiscord, FaGlobe } from "react-icons/fa6";
+import { FaCircleNotch } from "react-icons/fa6";
 
 type RepoOutput = {
   id: number;
@@ -75,42 +75,6 @@ export default function Projects() {
             ))
           )}
         </div>
-
-        <section className="closed-source-projects">
-          <h2>Closed-source projects</h2>
-
-          <div className="project-container">
-            <article className="project-card private-project-card">
-              <h3 className="project-title">theo's offsets</h3>
-              <p className="project-description">
-                A website providing accurate and always updated offsets for
-                Roblox externals.
-              </p>
-
-              <div className="project-bottom-container project-links">
-                <a
-                  className="project-link"
-                  href="https://offsets.imtheo.lol"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaGlobe size={13} />
-                  Website
-                </a>
-
-                <a
-                  className="project-link"
-                  href="https://discord.gg/rbxoffsets"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaDiscord size={14} />
-                  Discord
-                </a>
-              </div>
-            </article>
-          </div>
-        </section>
       </div>
     </>
   );
